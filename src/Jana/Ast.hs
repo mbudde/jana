d8aaf9c3b29ef43eab4db772afaaebb6194f870a
Michael Budde <mbudde@gmail.com>
2013-03-02 20:38:19 +0100

    Add draft abstract syntax tree

---
+++ src/Jana/Ast.hs
@@ -0,0 +1,66 @@
+
+module Jana.Ast where
+
+-- Data types
+data Type = Int
+          | Stack
+          deriving (Eq, Show, Read)
+
+-- Identifier
+type Ident = String
+
+-- Left-value
+data Lval = Scalar Ident
+          | Array  Ident Int
+          deriving (Eq, Show, Read)
+
+-- Modification operators used in assignment
+data ModOp = PlusEq
+           | MinusEq
+           | XorEq
+           deriving (Eq, Show, Read)
+
+-- Operator
+data Op = Plus          -- Arithmetic
+        | Minus
+        | Times
+        | Divide
+        | Modulo
+        | And           -- Binary
+        | Or
+        | Xor
+        | LAnd          -- Logical
+        | LOr
+        | GreaterThan   -- Relational
+        | LessThan
+        | Equal
+        | GreaterEqual
+        | LessEqual
+        deriving (Eq, Show, Read)
+
+data Statement = Assign   ModOp Lval Expr
+               | IfElse   Expr [Statement] [Statement] Expr
+               | FromLoop Expr [Statement] [Statement] Expr
+               | Push     Ident Ident
+               | Pop      Ident Ident
+               | Local    (Type, Ident, Expr) [Statement] (Type, Ident, Expr)
+               | Call     Ident [Ident]
+               | Uncall   Ident [Ident]
+               | Swap     Ident Ident
+               | Skip
+               deriving (Eq, Show, Read)
+
+data Expr = Const Int
+          | LV    Lval
+          | BinOp Op Expr Expr
+          | Empty Ident
+          | Top   Ident
+          | Nil
+          deriving (Eq, Show, Read)
+
+data Proc = Proc { procname  :: Ident
+                 , params    :: [(Type, Ident)]   -- Zero or more
+                 , body      :: [Statement] }
+          deriving (Eq, Show, Read)
+
+type Program = [Proc]
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
 
+import Jana.Ast
+
+fib = [ Proc { procname = "fib"
+             , params = [ (Int, "x1"), (Int, "x2"), (Int, "n") ]
+             , body = [ IfElse (BinOp Equal
+                                      (LV $ Scalar "n")
+                                      (Const 0))
+                               [ Assign PlusEq (Scalar "x1") (Const 1)
+                               , Assign PlusEq (Scalar "x2") (Const 1) ]
+                               [ Call "fib" [ "x1", "x2", "n" ]
+                               , Assign PlusEq (Scalar "x1") (LV $ Scalar "x2")
+                               , Swap "x1" "x2" ]
+                               (BinOp Equal
+                                      (LV $ Scalar "x1")
+                                      (LV $ Scalar "x2")) ] }
+
+      ] :: Program
 
 main :: IO ()
-main = print "foo"
+main = print fib
