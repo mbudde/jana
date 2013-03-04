9503f5422ce0f16c0bfc4fcfe0ea74354de33d72
Michael Budde <mbudde@gmail.com>
2013-03-04 12:50:45 +0100

    Add rest of fib program

--- src/Main.hs
+++ src/Main.hs
@@ -13,6 +13,15 @@ fib = [ Proc { procname = "fib"
                            (BinOp Eq (LV $ Scalar "x1")
                                      (LV $ Scalar "x2")) ] }
 
+      , Proc { procname = "fib_fwd"
+             , params = [ (Int, "x1"), (Int, "x2"), (Int, "n") ]
+             , body = [ Assign AddEq (Scalar "n") (Number 4)
+                      , Call "fib" [ "x1", "x2", "n" ] ] }
+      , Proc { procname = "fib_bwd"
+             , params = [ (Int, "x1"), (Int, "x2"), (Int, "n") ]
+             , body = [ Assign AddEq (Scalar "x1") (Number 5)
+                      , Assign AddEq (Scalar "x2") (Number 8)
+                      , Uncall "fib" [ "x1", "x2", "n" ] ] }
       ] :: Program
 
 main :: IO ()
