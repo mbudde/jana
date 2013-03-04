77a69e61c0ef3aa732d68f16a7c6dcbaa60cfa64
Claus Skou Nielsen <kalusn@kalusn-macbook.local>
2013-03-04 19:06:17 +0100

    Added new parserfile with some training/junk

--- src/Jana/Parser.hs
+++ src/Jana/Parser.hs
@@ -1,58 +1,39 @@
 module Jana.Parser where
 
-import Text.Parsec
-import Text.Parsec.String (Parser)
-import qualified Text.Parsec.Token as T
-
 import Jana.Ast
+import Text.Parsec
+import Text.Parsec.String
+import Text.Parsec.Expr
+import Text.Parsec.Token
+import Text.Parsec.Language
+
+def = emptyDef{
+    commentStart     = "(*",
+    commentEnd       = "*)",
+    identStart       = letter,
+    identLetter      = alphaNum,
+    opStart          = oneOf "+-^=",
+    opLetter         = oneOf "+-^=",
+    reservedOpNames  = ["+", "-", "^", "="],
+    reservedNames    = ["procedure",
+                        "int", "stack",
+                        "if", "then", "else", "fi",
+                        "from", "do", "loop", "until",
+                        "push", "pop",
+                        "local", "delocal",
+                        "call", "uncall",
+                        "skip",
+                        "empty", "top",
+                        "nil"]
+  }
+
+TokenParser{ parens     = m_parens
+           , identifier = m_identifier
+           , reservedOp = m_reservedOp
+           , reserved   = m_reserved
+           , semiSep1   = m_semiSep1
+           , whiteSpace = m_whiteSpace } = makeTokenParser def
 
 
-janusDef :: T.LanguageDef st
-janusDef = T.LanguageDef
-    { T.commentStart    = ""
-    , T.commentEnd      = ""
-    , T.commentLine     = "#"
-    , T.nestedComments  = True
-    , T.identStart      = letter <|> char '_'
-    , T.identLetter     = alphaNum <|> oneOf "_-"
-    , T.opStart         = T.opLetter janusDef
-    , T.opLetter        = oneOf "+-*/%&|<=>^"
-    , T.reservedOpNames = []
-    , T.reservedNames   = []
-    , T.caseSensitive   = True
-    }
-
-varType :: Parser Type
-varType =
-        (reserved "int" >> return Int)
-    <|> (reserved "stack" >> return Stack)
-
-param :: Parser (Type, Ident)
-param = do
-    parType <- varType
-    parName <- ident
-    return (parType, parName)
-
-paramList :: Parser [(Type, Ident)]
-paramList = commaSep param
-
-proc :: Parser Proc
-proc = do
-    reserved "procedure"
-    name <- ident
-    ps   <- parens paramList
-    return Proc { procname = name, params = ps, body = [] }
-
-parserTest =
-    case (parse proc "" "procedure foo(int x)") of
-        Left err -> error $ show err
-        Right p  -> print p
-
-
--- Lexer
-lexer = T.makeTokenParser janusDef
-
-ident    = T.identifier lexer
-reserved = T.reserved lexer
-parens   = T.parens lexer
-commaSep = T.commaSep lexer
+main :: IO ()
+main = play("hey")
