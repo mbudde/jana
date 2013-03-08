66540515ea11e2c03d81836a9f61d5c1c3a90893
Michael Budde <mbudde@gmail.com>
2013-03-08 15:08:43 +0100

    Add support for more operators

--- src/Jana/Interpreter.hs
+++ src/Jana/Interpreter.hs
@@ -1,5 +1,7 @@
 module Jana.Interpreter where
 
+import Prelude hiding (GT, LT, EQ)
+import Data.Bits
 import Control.Monad.State
 import qualified Data.Maybe as Maybe
 import qualified Data.Map as Map
@@ -59,6 +61,30 @@ initEvalEnv =
 type EvalComp a = State EvalEnv a
 
 
+boolToInt :: Bool -> Int
+boolToInt True  = 1
+boolToInt False = 0
+
+
+performOperation :: BinOp -> Int -> Int -> Int
+performOperation Add  = (+)
+performOperation Sub  = (-)
+performOperation Mul  = (*)
+performOperation Div  = undefined  -- FIXME
+performOperation Mod  = mod
+performOperation And  = (.&.)
+performOperation Or   = (.|.)
+performOperation Xor  = xor
+performOperation LAnd = undefined
+performOperation LOr  = undefined
+performOperation GT   = \x y -> boolToInt $ x > y
+performOperation LT   = \x y -> boolToInt $ x < y
+performOperation EQ   = \x y -> boolToInt $ x == y
+performOperation NEQ  = \x y -> boolToInt $ x /= y
+performOperation GE   = \x y -> boolToInt $ x >= y
+performOperation LE   = \x y -> boolToInt $ x <= y
+
+
 evalLval :: Lval -> EvalComp Value
 evalLval (Var id) = do
     storeEnv <- gets store
@@ -78,5 +104,5 @@ evalExpr (LV lval) = evalLval lval
 evalExpr (BinOp op e1 e2) = do
     VInt val1 <- evalExpr e1  -- FIXME: error when non-int
     VInt val2 <- evalExpr e2
-    return $ VInt (val1 + val2)
+    return $ VInt $ performOperation op val1 val2
 evalExpr Nil = return VNil
