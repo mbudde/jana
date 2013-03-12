6715f1ddce3d79e13c8cbc112bcc8edd92c95d90
Claus Skou Nielsen <kalusn@gmail.com>
2013-03-12 16:38:36 +0100

    Parser statements done

--- src/Jana/Parser.hs
+++ src/Jana/Parser.hs
@@ -4,13 +4,13 @@ import Prelude hiding (GT, LT, EQ)
 import System.IO
 import Control.Monad
 import Control.Applicative((<*))
+import Data.Maybe
 
 import Text.Parsec hiding (Empty)
 import Text.Parsec.String
 import Text.Parsec.Expr
 import Text.Parsec.Language
-import qualified Text.Parsec.Combinator as Combinator
-import qualified Text.Parsec.Token as Token
+import qualified Text.Parsec.Token      as Token
 
 import Jana.Ast
 
@@ -21,47 +21,47 @@ janaDef = Token.LanguageDef {
               , Token.nestedComments   = False
               , Token.identStart       = letter
               , Token.identLetter      = alphaNum
-              , Token.opStart          = oneOf "+-^*/%&|<>=!"
-              , Token.opLetter         = oneOf "&|="
-              , Token.reservedOpNames  = [ "+"
-                                   , "-"
-                                   , "^"
-                                   , "*"
-                                   , "/"
-                                   , "%"
-                                   , "&"
-                                   , "|"
-                                   , "&&"
-                                   , "||"
-                                   , "<"
-                                   , ">"
-                                   , "="
-                                   , "!="
-                                   , "<="
-                                   , ">="
-                                   ]
+              , Token.opStart          = oneOf "" -- oneOf "+-^*/%&|<>=!"
+              , Token.opLetter         = oneOf "" -- oneOf "&|="
+              , Token.reservedOpNames  = [{- "+"
+                                         , "-"
+                                         , "^"
+                                         , "*"
+                                         , "/"
+                                         , "%"
+                                         , "&"
+                                         , "|"
+                                         , "&&"
+                                         , "||"
+                                         , "<"
+                                         , ">"
+                                         , "="
+                                         , "!="
+                                         , "<="
+                                         , ">=" -}
+                                         ]
               , Token.reservedNames    = [ "procedure"
-                                   , "int"
-                                   , "stack"
-                                   , "if"
-                                   , "then"
-                                   , "else"
-                                   , "fi"
-                                   , "from"
-                                   , "do"
-                                   , "loop"
-                                   , "until"
-                                   , "push"
-                                   , "pop"
-                                   , "local"
-                                   , "delocal"
-                                   , "call"
-                                   , "uncall"
-                                   , "skip"
-                                   , "empty"
-                                   , "top"
-                                   , "nil"
-                                   ]
+                                         , "int"
+                                         , "stack"
+                                         , "if"
+                                         , "then"
+                                         , "else"
+                                         , "fi"
+                                         , "from"
+                                         , "do"
+                                         , "loop"
+                                         , "until"
+                                         , "push"
+                                         , "pop"
+                                         , "local"
+                                         , "delocal"
+                                         , "call"
+                                         , "uncall"
+                                         , "skip"
+                                         , "empty"
+                                         , "top"
+                                         , "nil"
+                                         ]
               , Token.caseSensitive    = True
   }
 
@@ -102,17 +102,97 @@ statement =   assignStmt
           <|> uncallStmt
           <|> swapStmt
           <|> skipStmt
+          <?> "statement"
 
-assignStmt = undefined
-ifStmt     = undefined
-fromStmt   = undefined
-pushStmt   = undefined
-popStmt    = undefined
-localStmt  = undefined
-callStmt   = undefined
-uncallStmt = undefined
-swapStmt   = undefined
-skipStmt   = undefined
+assignStmt :: Parser Stmt
+assignStmt =
+  do lval  <- lval
+     modop <- modOp
+     expr  <- expression
+     return $ Assign modop lval expr
+
+modOp :: Parser ModOp
+modOp =   (reservedOp "+=" >> return AddEq)
+      <|> (reservedOp "-=" >> return SubEq)
+      <|> (reservedOp "^=" >> return XorEq)
+
+ifStmt :: Parser Stmt
+ifStmt =
+  do reserved "if"
+     entrycond  <- expression
+     reserved "then"
+     stats1     <- many statement
+     stats2     <- option [] $ reserved "else" >> many1 statement
+     reserved "fi"
+     exitcond   <- expression
+     return $ If entrycond stats1 stats2 exitcond
+
+fromStmt :: Parser Stmt
+fromStmt =
+  do reserved "from"
+     entrycond <- expression
+     stats1    <- option [] $ reserved "do"   >> many1 statement
+     stats2    <- option [] $ reserved "loop" >> many1 statement
+     reserved "until"
+     exitcond  <- expression
+     return $ From entrycond stats1 stats2 exitcond
+
+pushStmt :: Parser Stmt
+pushStmt =
+  do reserved "push"
+     (x,y) <- parens $ do x <- identifier
+                          comma
+                          y <- identifier
+                          return (x,y)
+     return $ Push x y
+
+popStmt :: Parser Stmt
+popStmt =
+  do reserved "pop"
+     (x,y) <- parens $ do x <- identifier
+                          comma
+                          y <- identifier
+                          return (x,y)
+     return $ Pop x y
+
+localStmt :: Parser Stmt
+localStmt =
+  do reserved "local"
+     type1  <- atype
+     ident1 <- identifier
+     reservedOp "="
+     expr1  <- expression
+     stats  <- many statement
+     reserved "delocal"
+     type2  <- atype
+     ident2 <- identifier
+     reservedOp "="
+     expr2  <- expression
+     return $ Local (type1, ident1, expr1) stats (type2, ident2, expr2)
+
+atype :: Parser Type
+atype =   (reserved "int"   >> return Int)
+      <|> (reserved "stack" >> return Stack)
+
+callStmt :: Parser Stmt
+callStmt =
+  do reserved "call"
+     procname <- identifier
+     args     <- parens $ sepBy identifier comma
+     return $ Call procname args
+
+uncallStmt :: Parser Stmt
+uncallStmt =
+  do reserved "uncall"
+     procname <- identifier
+     args     <- parens $ sepBy identifier comma
+     return $ Uncall procname args
+
+swapStmt :: Parser Stmt
+swapStmt = reserved "swap" >> liftM2 Swap identifier identifier
+
+skipStmt :: Parser Stmt
+skipStmt = reserved "skip" >> return Skip
 
 expression :: Parser Expr
 expression =  buildExpressionParser binOperators term
@@ -123,7 +203,7 @@ term =   parens expression
      <|> lvalExpr
      <|> emptyExpr
      <|> topExpr
-     <|> return Nil
+     <?> "expression"
 
 numberExpr :: Parser Expr
 numberExpr = liftM Number integer
@@ -163,8 +243,8 @@ binOperators = [ [ Infix (reservedOp "*"   >> return (BinOp Mul )) AssocLeft
                ]
 
 
-parseString :: String -> Expr
+parseString :: String -> [Stmt]
 parseString str =
-  case parse expression "" str of
+  case parse (many statement) "" str of
     Left e  -> error $ show e
     Right r -> r
