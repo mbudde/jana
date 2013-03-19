1a1896f7169230d3107c655d63a5bb50d18e21d4
Michael Budde <mbudde@gmail.com>
2013-03-19 11:33:01 +0100

    Rework interpreter
    Move some definition to Types.hs and add new functions for getting and
    setting variables.

--- src/Jana/Eval.hs
+++ src/Jana/Eval.hs
@@ -1,82 +1,52 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
 module Jana.Eval where
 
 
 import Prelude hiding (GT, LT, EQ)
-import Data.Bits
 import Control.Monad
 import Control.Monad.State
-import Control.Monad.Reader
 import Control.Monad.Error
 
 import Jana.Ast
 import Jana.Types
 
 
-newtype Eval a = E { runE :: StateT Store (ReaderT ProcEnv (Either JError)) a }
-               deriving (Monad, MonadError JError, MonadReader ProcEnv, MonadState Store)
-
-runEval :: Eval a -> Store -> ProcEnv -> Either JError (a, Store)
-runEval eval store procs = runReaderT (runStateT (runE eval) store) procs
-
--- XXX: Implement monad instances manually?
-{- instance Monad Eval Store where -}
-    {- return = E . return -}
-    {- e >>= f = S -}
-
-
 unpackInt :: Value -> Eval Integer
 unpackInt (JInt x) = return x
-unpackInt (JArray _) = throwError $ TypeMismatch "int" "array"
-unpackInt (JStack _) = throwError $ TypeMismatch "int" "stack"
+unpackInt val = throwError $ TypeMismatch "int" (showValueType val)
+
+unpackArray :: Value -> Eval Array
+unpackArray (JArray x) = return x
+unpackArray val = throwError $ TypeMismatch "array" (showValueType val)
+
+unpackStack :: Value -> Eval Stack
+unpackStack (JStack x) = return x
+unpackStack val = throwError $ TypeMismatch "stack" (showValueType val)
 
 
-boolToInt :: Num a => (a -> a -> Bool) -> a -> a -> a
-boolToInt f x y = if f x y then 1 else 0
-
-
-performOperation :: (Integral a, Bits a) => BinOp -> a -> a -> a
-performOperation Add  = (+)
-performOperation Sub  = (-)
-performOperation Mul  = (*)
-performOperation Div  = undefined  -- FIXME
-performOperation Mod  = mod
-performOperation And  = (.&.)
-performOperation Or   = (.|.)
-performOperation Xor  = xor
-performOperation LAnd = undefined
-performOperation LOr  = undefined
-performOperation GT   = boolToInt (>)
-performOperation LT   = boolToInt (<)
-performOperation EQ   = boolToInt (==)
-performOperation NEQ  = boolToInt (/=)
-performOperation GE   = boolToInt (>=)
-performOperation LE   = boolToInt (<=)
+arrayLookup :: Array -> Integer -> Eval Value
+arrayLookup arr idx =
+  if idx < 0 || idx' >= length arr
+    then throwError OutOfBounds
+    else return $ JInt $ arr !! idx'
+  where idx' = fromInteger idx
 
 
 evalLval :: Lval -> Eval Value
-evalLval (Var id) =
-  do storeEnv <- get
-     return $ lookupIdent id storeEnv
+evalLval (Var id) = getVar id
 evalLval (Lookup id e) =
-  do storeEnv <- get
-     JInt idx <- evalExpr e  -- FIXME: error
-     let JArray arr = lookupIdent id storeEnv in
-       if toInteger (length arr) <= idx || idx < 0
-         then error "out of bounds"
-         else return $ JInt (arr !! fromInteger idx)
-
+  do idx <- unpackInt =<< evalExpr e  -- FIXME: error
+     arr <- unpackArray =<< getVar id
+     arrayLookup arr idx
 
 
 evalExpr :: Expr -> Eval Value
 evalExpr (Number x) = return $ JInt x
-evalExpr Nil        = return $ nil
+evalExpr Nil        = return nil
 evalExpr (LV lval)  = evalLval lval
 evalExpr (BinOp op e1 e2) =
-  do x <- unpackInt =<< evalExpr e1
-     y <- unpackInt =<< evalExpr e2
-     return $ JInt $ performOperation op x y
+  do x <- evalExpr e1
+     y <- evalExpr e2
+     performOperation op x y
 evalExpr (Top _)    = undefined
 evalExpr (Empty _)  = undefined
 
--- src/Jana/Types.hs
+++ src/Jana/Types.hs
@@ -1,24 +1,34 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Jana.Types (
-    Value(..), nil,
+    Array, Stack,
+    Value(..), nil, performOperation, showValueType,
     JError(..),
-    Store, emptyStore, bindIdent, lookupIdent,
+    Store, emptyStore, setVar, getVar,
     ProcEnv, emptyProcEnv, bindProc, lookupProc,
+    Eval,
     ) where
 
+import Prelude hiding (GT, LT, EQ)
+import Data.Bits
 import Data.List (intercalate)
+import Control.Monad.State
+import Control.Monad.Reader
 import Control.Monad.Error
 import qualified Data.Maybe as Maybe
 import qualified Data.Map as Map
 
-import Jana.Ast (Ident, Proc)
+import Jana.Ast
 
+type Array = [Integer]
+type Stack = [Integer]
 
 -- Types of values an expression can evaluate to.
 data Value
-    = JInt Integer
-    | JArray [Integer]
-    | JStack [Integer]
-    deriving (Eq)
+  = JInt Integer
+  | JArray Array
+  | JStack Stack
+  deriving (Eq)
 
 instance Show Value where
   show (JInt x)    = show x
@@ -26,40 +36,63 @@ instance Show Value where
   show (JStack []) = "nil"
   show (JStack xs) = "<" ++ intercalate "," (map show xs) ++ "]"
 
+showValueType :: Value -> String
+showValueType (JInt _) = "int"
+showValueType (JStack _) = "stack"
+showValueType (JArray _) = "array"
+
 nil = JStack []
 
-data JError
-    = UnboundVar String       -- FIXME: Add source pos
-    | TypeMismatch String String
-    | UndefProc String
-    | Unknown String
 
-instance Show JError where
-  show (UnboundVar name) = "variable not declared: " ++ name
-  show (TypeMismatch expType foundType) = "expected type " ++ expType ++
-                                          " but got " ++ foundType
-  show (UndefProc name) = "function not defined: " ++ name
-  show (Unknown s) = "unknown error: " ++ s
+boolToInt :: Num a => (a -> a -> Bool) -> a -> a -> a
+boolToInt f x y = if f x y then 1 else 0
 
-instance Error JError where
-  noMsg  = Unknown "Unknown error"
-  strMsg = Unknown
 
+getOperatorFunc :: (Integral a, Bits a) => BinOp -> a -> a -> a
+getOperatorFunc Add  = (+)
+getOperatorFunc Sub  = (-)
+getOperatorFunc Mul  = (*)
+getOperatorFunc Div  = undefined  -- FIXME
+getOperatorFunc Mod  = mod
+getOperatorFunc And  = (.&.)
+getOperatorFunc Or   = (.|.)
+getOperatorFunc Xor  = xor
+getOperatorFunc LAnd = undefined
+getOperatorFunc LOr  = undefined
+getOperatorFunc GT   = boolToInt (>)
+getOperatorFunc LT   = boolToInt (<)
+getOperatorFunc EQ   = boolToInt (==)
+getOperatorFunc NEQ  = boolToInt (/=)
+getOperatorFunc GE   = boolToInt (>=)
+getOperatorFunc LE   = boolToInt (<=)
+
+
+performOperation :: BinOp -> Value -> Value -> Eval Value
+performOperation op (JInt x) (JInt y) =
+  return $ JInt $ getOperatorFunc op x y
+performOperation _ (JStack _) val =
+  throwError $ TypeMismatch "int" (showValueType val)
+performOperation _ val _ =
+  throwError $ TypeMismatch "int" (showValueType val)
+
+
+--
+-- Environment
+--
 
 type Store = Map.Map Ident Value
 
 emptyStore = Map.empty
 
-bindIdent :: Ident -> Value -> Store -> Store
-bindIdent id val store
-    = if Map.notMember id store
-        then Map.insert id val store
-        else error $ "variable already declared"
+setVar :: Ident -> Value -> Eval ()
+setVar id val = modify $ Map.insert id val
 
-lookupIdent :: Ident -> Store -> Value
-lookupIdent id store
-    = Maybe.fromMaybe (error $ "identifier not found")
-                      (Map.lookup id store)
+getVar :: Ident -> Eval Value
+getVar id =
+  do storeEnv <- get
+     case Map.lookup id storeEnv of
+       Just val -> return val
+       Nothing  -> throwError $ UnboundVar id
 
 
 type ProcEnv = Map.Map Ident Proc
@@ -68,9 +101,45 @@ emptyProcEnv = Map.empty
 
 bindProc :: Ident -> Proc -> ProcEnv -> ProcEnv
 bindProc name proc env
-    = if Map.notMember name env
-        then Map.insert name proc env
-        else error "function already defined"
+  = if Map.notMember name env
+      then Map.insert name proc env
+      else error "function already defined"
 
 lookupProc :: Ident -> ProcEnv -> Maybe Proc
 lookupProc = Map.lookup
+
+
+--
+-- Evaluation
+--
+
+newtype Eval a = E { runE :: StateT Store (ReaderT ProcEnv (Either JError)) a }
+               deriving (Monad, MonadError JError, MonadReader ProcEnv, MonadState Store)
+
+runEval :: Eval a -> Store -> ProcEnv -> Either JError (a, Store)
+runEval eval store procs = runReaderT (runStateT (runE eval) store) procs
+
+-- XXX: Implement monad instances manually?
+{- instance Monad Eval Store where -}
+    {- return = E . return -}
+    {- e >>= f = S -}
+
+
+data JError       -- FIXME: Add source pos
+  = UnboundVar   String           -- ident
+  | TypeMismatch String String    -- expected-type found-type
+  | OutOfBounds
+  | UndefProc    String           -- ident
+  | Unknown      String           -- message
+
+instance Show JError where
+  show (UnboundVar name) = "variable not declared: " ++ name
+  show (TypeMismatch expType foundType) = "expected type " ++ expType ++
+                                          " but got " ++ foundType
+  show (OutOfBounds) = "array lookup was out of bounds"
+  show (UndefProc name) = "function not defined: " ++ name
+  show (Unknown s) = "unknown error: " ++ s
+
+instance Error JError where
+  noMsg  = Unknown "Unknown error"
+  strMsg = Unknown
