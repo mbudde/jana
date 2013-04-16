d15c0770c80c3ac3cfd7b88698815c65976525e5
Michael Budde <mbudde@gmail.com>
2013-04-16 14:34:59 +0200

    Import Set as qualified

--- src/Jana/Aliases.hs
+++ src/Jana/Aliases.hs
@@ -1,10 +1,17 @@
-module Jana.Aliases where
+module Jana.Aliases (
+  AliasSet,
+  empty,
+  introduce, propagate, mergeAliases
+  ) where
 
-import Data.Set
 import Data.Maybe (catMaybes)
+import qualified Data.Set as Set
 
 
-type AliasSet = Set (String, String)
+type AliasSet = Set.Set (String, String)
+
+empty :: AliasSet
+empty = Set.empty
 
 uniquePairs [] = []
 uniquePairs (x:xs) = [ (x, y) | y <- xs ] ++ uniquePairs xs
@@ -14,12 +21,15 @@ introduce xs =
   findDuplicates (uniquePairs xs)
   where findDuplicates [] = empty
         findDuplicates (((x, x'), (y, y')):xs) =
-          (if x == y then insert (x', y') else id) $ findDuplicates xs
+          (if x == y then Set.insert (x', y') else id) $ findDuplicates xs
 
 propagate :: [(String, String)] -> AliasSet -> AliasSet
-propagate xs aliases = Data.Set.foldr b empty aliases
-  where b (x, y) ys = fromList [ (a, b) | (x', a) <- xs
-                                        , (y', b) <- xs
-                                        , x == x' && y == y' ]
-                      `union` ys
+propagate xs aliases = Set.foldr b empty aliases
+  where b (x, y) ys = Set.fromList [ (a, b) | (x', a) <- xs
+                                            , (y', b) <- xs
+                                            , x == x' && y == y' ]
+                      `Set.union` ys
 
+mergeAliases :: [(String, String)] -> AliasSet -> AliasSet
+mergeAliases xs aliases =
+  introduce xs `Set.union` propagate xs aliases
