853fdbcf2e4804415c33c2d35de8c31ac8a6fa57
Michael Budde <mbudde@gmail.com>
2013-04-25 16:27:54 +0200

    Highlight gutter in lines with errors

--- web/static/js/playground.js
+++ web/static/js/playground.js
@@ -12,8 +12,31 @@ $(function(){
       $.get("examples/" + match[1] + ".ja", function(data) {
         editor.setValue(data);
         editor.gotoLine(0);
+        removeErrorMarkers();
       });
     }
+  };
+
+  var prevErrors = [];
+  var removeErrorMarkers = function() {
+    var session = editor.getSession();
+    for (var i = 0; i < prevErrors.length; ++i) {
+      session.removeGutterDecoration(prevErrors[i], "errorGutter");
+      prevErrors = [];
+    }
+  };
+
+  var formatOutput = function(output) {
+    var session = editor.getSession();
+    var match = output.match(/line (\d+), column (\d+)/);
+
+    removeErrorMarkers();
+    if (match) {
+      var line = parseInt(match[1]) - 1;
+      session.addGutterDecoration(line, "errorGutter");
+      prevErrors.push(line);
+    }
+    return $("<pre>").html(output);
   };
 
   $("#examples a").click(function(e) {
@@ -28,7 +51,7 @@ $(function(){
     $.post("execute.php", {
       "code": code
     }, function(res) {
-      $("#output").empty().append($("<pre>").html(res));
+      $("#output").empty().append(formatOutput(res));
     });
   });
 
