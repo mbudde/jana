7df14ca659bd4af8b4a4075592912e87f0f7bdc0
Claus Skou Nielsen <kalusn@gmail.com>
2013-05-30 14:08:42 +0200

    Fixed global inversion so it does not invert call/uncall

--- src/Jana/Invert.hs
+++ src/Jana/Invert.hs
@@ -4,34 +4,41 @@ import Jana.Ast
 
 invertProgram :: Program -> Program
 invertProgram (Program mains procs) =
-  Program mains (map invertProc procs)
+  Program mains (map invertProcGlobally procs)
 
 invertProc :: Proc -> Proc
-invertProc proc = proc { body = invertStmts $ body proc }
+invertProc proc = proc { body = invertStmts False $ body proc }
 
-invertStmts :: [Stmt] -> [Stmt]
-invertStmts = reverse . map invertStmt
+invertProcGlobally :: Proc -> Proc
+invertProcGlobally proc = proc { body = invertStmts True $ body proc } 
 
-invertStmt :: Stmt -> Stmt
-invertStmt (Assign modOp lval expr pos) =
+invertStmts :: Bool -> [Stmt] -> [Stmt]
+invertStmts global = reverse . map (invertStmt global)
+
+invertStmt :: Bool -> Stmt -> Stmt
+invertStmt _ (Assign modOp lval expr pos) =
   Assign (invertModOp modOp) lval expr pos
   where invertModOp AddEq = SubEq
         invertModOp SubEq = AddEq
         invertModOp XorEq = XorEq
-invertStmt (If e1 ifPart elsePart e2 pos) =
-  If e2 (invertStmts ifPart) (invertStmts elsePart) e1 pos
-invertStmt (From e1 doPart loopPart e2 pos) =
-  From e2 (invertStmts doPart) (invertStmts loopPart) e1 pos
-invertStmt (Push id1 id2 pos) = Pop  id1 id2 pos
-invertStmt (Pop  id1 id2 pos) = Push id1 id2 pos
-invertStmt (Local assign1 body assign2 pos) =
-  Local assign2 (invertStmts body) assign1 pos
-invertStmt (Call funId args pos) =
+invertStmt global (If e1 ifPart elsePart e2 pos) =
+  If e2 (invertStmts global ifPart) (invertStmts global elsePart) e1 pos
+invertStmt global (From e1 doPart loopPart e2 pos) =
+  From e2 (invertStmts global doPart) (invertStmts global loopPart) e1 pos
+invertStmt _ (Push id1 id2 pos) = Pop  id1 id2 pos
+invertStmt _ (Pop  id1 id2 pos) = Push id1 id2 pos
+invertStmt global (Local assign1 body assign2 pos) =
+  Local assign2 (invertStmts global body) assign1 pos
+invertStmt True stmt@(Call{}) =
+  stmt
+invertStmt False (Call funId args pos) =
   Uncall funId args pos
-invertStmt (Uncall funId args pos) =
+invertStmt True stmt@(Uncall{}) =
+  stmt
+invertStmt False (Uncall funId args pos) =
   Call funId args pos
-invertStmt stmt@(UserError{}) = stmt
-invertStmt stmt@(Swap{}) = stmt
-invertStmt stmt@(Prints{}) = stmt
-invertStmt (Skip pos) = Skip pos
+invertStmt _ stmt@(UserError{}) = stmt
+invertStmt _ stmt@(Swap{}) = stmt
+invertStmt _ stmt@(Prints{}) = stmt
+invertStmt _ stmt@(Skip{}) = stmt
 
